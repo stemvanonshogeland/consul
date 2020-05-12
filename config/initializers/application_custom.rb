0e1ac77dc7ef7cdbbd96a9d7194ac50af74c77dc
Julian Herrero <microweb10@gmail.com>
2020-05-12 10:22:24 +0700

    Use only dutch language

--- config/initializers/application_custom.rb
+++ config/initializers/application_custom.rb
@@ -1,7 +1,8 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :nl
-    config.i18n.available_locales = ["nl"]
-    config.i18n.available_locales = ["en", "es", "fr", "gl", "it", "nl", "pt-BR"] if Rails.env.test?
+    unless Rails.env.test?
+      config.i18n.default_locale = :nl
+      config.i18n.available_locales = ["nl"]
+    end
   end
 end
