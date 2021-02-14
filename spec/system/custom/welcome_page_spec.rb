6b34cc7654cb33ee1ba12ec3a467331b8951f9c7
decabeza <alberto@decabeza.es>
2021-02-14 11:12:15 +0100

    Show all published budgets on welcome page

--- spec/system/custom/welcome_page_spec.rb
+++ spec/system/custom/welcome_page_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Welcome page" do
   context "Feeds" do
-    scenario "Show open published budgets info" do
+    scenario "Show published budgets info" do
       budget = create(:budget, :accepting)
       finished = create(:budget, :finished)
       draft = create(:budget, :drafting)
@@ -18,14 +18,18 @@ describe "Welcome page" do
         expect(page).to have_content "#{budget.start_date.to_date}"
         expect(page).to have_content "#{budget.end_date.to_date}"
         expect(page).to have_content budget.description
-        expect(page).to have_content "See this budget"
+        expect(page).to have_content "See this budget", count: 2
         expect(page).to have_link href: budget_path(budget)
+        expect(page).to have_content finished.name
+        expect(page).to have_content finished.formatted_total_headings_price
+        expect(page).to have_content "Completed"
+        expect(page).to have_content "#{finished.start_date.to_date}"
+        expect(page).to have_content "#{finished.end_date.to_date}"
+        expect(page).to have_content finished.description
+        expect(page).to have_link href: budget_path(finished)
         expect(page).not_to have_content draft.name
         expect(page).not_to have_content draft.description
         expect(page).not_to have_link href: budget_path(draft)
-        expect(page).not_to have_content finished.name
-        expect(page).not_to have_content finished.description
-        expect(page).not_to have_link href: budget_path(finished)
       end
     end
   end
