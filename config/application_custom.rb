34b6bb8ad04731d282407d9254d5be3d0154d133
Julian Herrero <microweb10@gmail.com>
2022-03-23 11:11:43 +0700

    TMP

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,11 @@
 module Consul
   class Application < Rails::Application
+    config.x.theme.current = "onshogeland"
+    config.x.theme.load_path = Rails.root.join(
+      "app",
+      "assets",
+      "stylesheets",
+      "themes"
+    )
   end
 end
